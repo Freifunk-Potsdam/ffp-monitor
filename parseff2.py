5e532c6ca9472d60c7e8f258dc1d75fb3a300a4c
root <root@freifunk-monitor>
2017-01-12 15:15:00 +0100

    narrowing down changes on dicts

--- parseff2.py
+++ parseff2.py
@@ -2,7 +2,7 @@
 from base import *
 import time
 import sys
-from pprint import pprint
+from pprint import pprint,pformat
 
 allhosts = set( mongodb["nodes"].distinct("hostname") + mongodb["tmpnodeinfo"].distinct("host") )
 for host in allhosts:
@@ -11,8 +11,7 @@ for host in allhosts:
     new = list( mongodb["tmpnodeinfo"].find({ "host": host, "time": { "$gt": last_ts } }, sort = [("time",1)] ) )
     if len(new) > 0:
         if old is None:
-            print("New Host %s" % host)
-            mongodb["changes"].insert({ "host": host, "time": time.time(), "ctime": t, "param": "hostname", "new": host, "old": None })
+            mongodb["changes"].insert({ "host": host, "time": time.time(), "ctime": new[0]["time"], "param": "hostname", "new": host, "old": None })
             old = {}
         for n in new:
             n.pop("_id")
@@ -27,11 +26,52 @@ for host in allhosts:
                     v.pop("tx_packets",None)
                     v.pop("rx_bytes",None)
                     v.pop("tx_bytes",None)
+                    if k.startswith("tnl_"):
+                        v["mac"] = None
                 n["ifc"] = nifc
             for a,newv in list(n.items()):
                 oldv = old.get(a,None)
-                if newv != oldv:
-                    mongodb["changes"].insert({ "host": host, "time":time.time(), "ctime": t, "param": a, "new": newv, "old": oldv })
+                changed = False
+                detail = None
+                if type(oldv) != type(newv):
+                    changed = True
+                elif isinstance(oldv,dict):
+                    added = []
+                    removed = []
+                    modified = []
+                    allkeys = set( oldv.keys() ) | set( newv.keys() )
+                    for k in allkeys:
+                        if k not in oldv:
+                            added.append( k )
+                        elif k not in newv:
+                            removed.append( k )
+                        elif oldv[k] != newv[k]:
+                            modified.append( k )
+                    if len(added+removed+modified) > 0:
+                        detail = {"modified":modified,"added":added,"removed":removed,"old":oldv,"new":newv}
+                        oldl = []
+                        newl = []
+                        for k in sorted(modified):
+                            fmtol = [ "|" + l[1:] for l in pformat({k:oldv[k]})[:-1].split("\n") ]
+                            fmtnl = [ "|" + l[1:] for l in pformat({k:newv[k]})[:-1].split("\n") ]
+                            oldl.extend(fmtol)
+                            newl.extend(fmtnl)
+                        for k in sorted(removed):
+                            fmtl = [ "-" + l[1:] for l in pformat({k:oldv[k]})[:-1].split("\n") ]
+                            oldl.extend(fmtl)
+                        for k in sorted(added):
+                            fmtl = [ "+" + l[1:] for l in pformat({k:newv[k]})[:-1].split("\n") ]
+                            newl.extend(fmtl)
+                        oldv = "\n".join(oldl)
+                        newv = "\n".join(newl)
+                        changed = True
+                else:
+                    changed = oldv != newv
+                if changed:
+                    c = { "host": host, "time":time.time(), "ctime": t, "param": a, "new": newv, "old": oldv }
+                    if detail is not None:
+                        c["detail"] = detail
+                    mongodb["changes"].insert( c )
                 else:
                     n.pop(a)
             q = {   
@@ -58,7 +98,7 @@ for host in mongodb["nodes"].find():
     if new != old:
         mongodb["changes"].insert({
             "host": host["hostname"],
-            "time":now,
+            "time": now,
             "ctime": now,
             "param": "state",
             "new": new,
@@ -70,8 +110,9 @@ mongodb["tmpnodeinfo"].remove({ "time": { "$lt": now - 24 * 60 * 60 } })
 mongodb["changes"].remove({ "time": { "$lt": now - 7 * 24 * 60 * 60 } })
 # remove nodes, not seen for 35 days
 for n in mongodb["nodes"].find({ "last_ts": { "$lt": now - 35 * 24 * 60 * 60 } }):
-    mongodb["nodes"].remove(n["_id"])
-    mongodb["changes"].insert({ "host": host, "time": time.time(), "ctime": time.time(), "param": "hostname", "new": None, "old": n["hostname"] })
+    mongodb["nodes"].delete_one( {"_id":n["_id"]} )
+    mongodb["names"].delete_many( {"hostname":n["hostname"]} )
+    mongodb["changes"].insert({ "host": n["hostname"], "time": time.time(), "ctime": time.time(), "param": "hostname", "new": None, "old": n["hostname"] })
 
 # update uptimes
 infq = 'SELECT last("uptime") AS "uptime" FROM "load" ' + \
