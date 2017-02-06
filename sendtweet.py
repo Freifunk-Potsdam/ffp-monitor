5489e80df78327c11af3b0b087b7c23a6762b827
root <root@freifunk-monitor>
2017-02-06 11:19:16 +0100

    complete refactoring

--- sendtweet.py
+++ sendtweet.py
@@ -2,6 +2,7 @@
 from base import *
 import time
 from twitter import *
+import traceback
 import random
 random.seed()
 
@@ -9,25 +10,46 @@ from twitter_auth import *
 
 t = Twitter( auth = OAuth( access_token, access_token_secret, consumer_key, consumer_secret ))
 
-for e in mongodb["events"].find({"time":{"$lte":time.time()},"sent":{"$exists":False}}):
-    tpls = list(mongodb["templates"].find({"type":e["type"]}))
+def sendtweet(twitter, msg):
+    if len(msg) > 140:
+        msg = msg[:137] + "..."
+    try:
+        t.statuses.update( status = msg )
+        return True
+    except TwitterHTTPError as ex:
+        errs = ex.response_data.get("errors",[])
+        for err in errs:
+            if err.get("code",0) == 187:
+                # duplicate, not sending again
+                return None
+        traceback.print_exc(file=sys.stdout)
+        return False
+
+now = time.time()
+last = mongodb["tweets"].find_one({},sort=[("ctime",-1)])
+last = now - 3600 if last is None else last["ctime"]
+for c in mongodb["changes"].find( { "$and":[ { "time": { "$gt": last } }, { "time": { "$lte": now } } ] }, sort = [("time",1)] ):
+    q = {
+        "param":c["param"],
+        "$and": [
+            { "$or": [
+                {"old":{"$exists":False}},
+                {"old":c["old"]},
+            ]},
+            { "$or": [
+                {"new":{"$exists":False}},
+                {"new":c["new"]},
+            ]},
+            { "$or": [
+                {"modified":{"$exists":False}},
+                {"modified":{"$in": c.get("detail",{}).get("modified",[]) }},
+            ]},
+        ],
+    }
+    tpls = list(mongodb["templates"].find( q ))
     if len(tpls) > 0:
-        sent = False
-        msg = random.choice(tpls)["msg"].format_map(e)
-        if len(msg) > 140:
-            msg = msg[:137] + "..."
-        try:
-            t.statuses.update( status = msg )
-            sent = True
-        except TwitterHTTPError as ex:
-            errs = ex.response_data.get("errors",[])
-            for err in errs:
-                if err.get("code",0) == 187:
-                    # duplicate, not sending again
-                    sent = True
-            if not sent:
-                raise ex
-        if sent:
-            mongodb["events"].update({"_id":e["_id"]},{"$set":{"sent":time.time()}})
-    else:
-        print("No template of type %s found." % e["type"])
+        msg = random.choice(tpls)["msg"].format_map(c)
+        if sendtweet(t, msg) is not False:
+#            print(msg)
+            mongodb["tweets"].insert({ "msg": msg, "sent": now, "ctime": c["time"] })
+            break
